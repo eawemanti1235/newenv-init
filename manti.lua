721ee6745c5618368706f988344da9e98099634d
rbxmanti <130973542+rbxmanti@users.noreply.github.com>
2024-08-15 12:26:59 +0300

    Create manti.lua

---
+++ manti.lua
@@ -0,0 +1,7 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{
+    Title = "MantiWPF",
+    Icon = "rbxassetid://18956441295",
+    Text = "version v3.0.0",
+    Duration = 5    
+})
+print("MantiWPF is injected!")
