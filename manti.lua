b47cd32c8729cb019bcb941b6590a9d235f2763e
eawemanti1235 <130973542+eawemanti1235@users.noreply.github.com>
2024-08-24 17:49:32 +0300

    Update manti.lua

--- manti.lua
+++ manti.lua
@@ -6,3 +6,15 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
     Duration = 7    
 })
 print("MantiWPF is injected!")
+getgenv().getcustomasset = function(path)
+    assert(path ~= "", `arg #1 cannot be empty`)
+    assert(type(path) == "string", `arg #1 must be type string`)
+
+    local backslashed_path = string.gsub(path, "/", "\\")
+    local success, asset_url = getcustomassetTools(backslashed_path)
+
+    if not success then
+        return error(`Failed to get asset URL of '{path}'`, 2)
+    end
+    return asset_url
+end
